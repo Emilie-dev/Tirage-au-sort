f24641489a9ed7337251548daed733373c4045d7
Chauvin <chauvin-e@orange.fr>
2017-04-03 12:36:26 +0200

    Création d'un bouton pour générer des chiffres aléatoire avec Math.random.

---
+++ app.js
@@ -0,0 +1,43 @@
+var person = ["Julien", "Dimitri", "Marco", "Raphael", "Mouad", "Jordy", "Maxime", "Romain", "Océane", "Morel", "Grégory", "Frank", "Florian", "Elodie", "Emilie"];
+console.log(person);
+
+
+var table_1 = [];
+
+var table_2 = [];
+
+var table_3 = [];
+
+var table_4 = [];
+
+var table_5 = []; 
+
+
+
+$("#execute").click(function () {
+for (i = 0; i < person.length; i++) {
+	min = Math.ceil(0);
+  	max = Math.floor(14);
+
+var tirage = Math.floor(Math.random() * (max - min +1)) + min;
+console.log(tirage);
+
+
+
+}
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+ 
